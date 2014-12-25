d49924962d7025d1c10a7c3be945ca56f3c4c57d
Diego <ghecho@gmail.com>
2014-12-25 12:18:41 -0600

    Initial Commit

---
+++ EditableTableViewController-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EditableTableViewController-Swift
+//
+//  Created by Diego on 12/25/14.
+//  Copyright (c) 2014 Diego. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
